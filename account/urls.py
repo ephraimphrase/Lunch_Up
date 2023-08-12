35ce87ca62096159c0af8cea08a476ad417eb3c1
ephraimphrase <ephraimphrase@gmail.com>
2023-08-12 04:20:11 +0100

    Fixed access and refresh token functionalities

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from django.views.decorators.csrf import csrf_exempt
+
 
 urlpatterns = [
     path('api/register/', views.RegisterView.as_view()),
-    path('api/login/', views.LoginView.as_view())
+    path('api/login/', views.CustomTokenPairView.as_view()),
+    path('api/refresh/', views.CustomTokenRefreshView.as_view())
 ]
--- account/views.py
+++ account/views.py
@@ -9,10 +9,18 @@ from django.conf import settings
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import authentication_classes, permission_classes
 from datetime import datetime, timedelta
-from django.views.decorators.csrf import csrf_exempt
 from .utils import SessionCsrfExemptAuthentication
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
+class CustomTokenPairView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+
+class CustomTokenRefreshView(TokenRefreshView):
+    permission_classes = (AllowAny,)
+
 class RegisterView(APIView):
     def post(self, request):
         username = request.data.get('username')
@@ -27,50 +35,10 @@ class RegisterView(APIView):
             user = User.objects.create_user(username=username, password=password, email=email)
             user.save()
 
-            expiration_time = datetime.utcnow() + timedelta(hours=1)
-
-            # Create the payload containing the user data and expiration time
-            payload = {
-                'user_id': user.id,
-                'username': user.username,
-                'exp': expiration_time,
-            }
-
-            # Generate the JWT token using the SECRET_KEY from Django settings
-            token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
-
-            login(request, user)
+            token = str(RefreshToken.for_user(user))
 
             return Response({"token":token}, status=status.HTTP_201_CREATED)
 
-@authentication_classes([SessionCsrfExemptAuthentication])
-class LoginView(APIView):
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-
-        try:
-            user = User.objects.get(username=username)
-
-            user = authenticate(request, username=username, password=password)
-
-            if user is not None:
-                expiration_time = datetime.utcnow() + timedelta(hours=1)
-
-                # Create the payload containing the user data and expiration time
-                payload = {
-                    'user_id': user.id,
-                    'username': user.username,
-                    'exp': expiration_time,
-                }
-
-                # Generate the JWT token using the SECRET_KEY from Django settings
-                token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
-
-                login(request, user)
-                return Response({"message":"Logged In successfully", "token":token}, status=status.HTTP_200_OK)
-        except:
-            return Response({"error":"User does not exist"}, status=status.HTTP_404_NOT_FOUND)
         
 
 
